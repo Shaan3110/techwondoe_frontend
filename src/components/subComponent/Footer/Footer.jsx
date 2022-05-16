f5e3c281b426710c7aca4c71d3889a91a474ad39
Shaan3110 <hitarasigan0987654321@gmail.com>
2022-05-16 23:25:00 +0530

    contact us done

---
+++ src/components/subComponent/Footer/Footer.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Footer.css";
+import { Typography } from "../../UI/Typography/Typography";
+import { Button } from "../../UI/Button/Button";
+
+export const ContactUs = () => {
+  return (
+    <div className="contactus">
+      <Typography
+        size={"heading"}
+        primary={false}
+        text={"Speak to a Career Expert"}
+      />
+      <div className="connectus_tagline">
+        <Typography
+          size={"paragraph"}
+          primary={false}
+          text={
+            "We have are dedicated to finding the best fit for you. Get in touch to organise a face-to-face cactch up with somone from our team"
+          }
+        />
+      </div>
+      <Button primary={false} size={"large"} label={"Get in touch"} />
+    </div>
+  );
+};
